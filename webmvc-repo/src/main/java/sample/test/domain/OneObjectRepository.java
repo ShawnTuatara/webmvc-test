57756c3a1eb3060ee9e2629030dabc9ab1e66117
Shawn Clark <shawn.tuatara@gmail.com>
2016-08-11 03:13:15 -0700

    Making the test pass

--- webmvc-repo/src/main/java/sample/test/domain/OneObjectRepository.java
+++ webmvc-repo/src/main/java/sample/test/domain/OneObjectRepository.java
@@ -1,7 +1,9 @@
 package sample.test.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OneObjectRepository extends CrudRepository<OneObject, Long> {
 
 }
--- webmvc-test/src/test/java/sample/test/web/MyControllerTest.java
+++ webmvc-test/src/test/java/sample/test/web/MyControllerTest.java
@@ -1,5 +1,10 @@
 package sample.test.web;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,25 +14,20 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(MyController.class)
 public class MyControllerTest {
 
-   @Autowired
-   private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-   @MockBean
-   private MyService ms;
+	@MockBean
+	private MyService ms;
 
-   @Test
-   public void getDataAndExpectOkStatus() throws Exception {
-      given(ms.execute("1")).willReturn(false);
-      mvc.perform(get("/1/data").accept(MediaType.APPLICATION_JSON_VALUE))
-              .andExpect(status().isOk()).andExpect(content().string("false"));
-   }
+	@Test
+	public void getDataAndExpectOkStatus() throws Exception {
+		given(ms.execute("1")).willReturn(false);
+		mvc.perform(get("/1/data").accept(MediaType.APPLICATION_JSON_VALUE)).andExpect(status().isOk())
+				.andExpect(content().string("false"));
+	}
 }
